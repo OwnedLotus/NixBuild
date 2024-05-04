48b0d7b0472850a20f232e2685a0c73e15319543
Jonah Ringdahl <46978061+OwnedLotus@users.noreply.github.com>
2024-05-03 22:05:04 -0400

    Update configuration.nix
    Add flatpak enable

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  services.flatpak.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
